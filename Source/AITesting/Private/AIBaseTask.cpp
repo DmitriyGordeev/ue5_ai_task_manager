2b41de8ef7b073f66d70bb693733224f82a04c5f
Dimaster <gordeev.dmitriy@physics.msu.ru>
2023-07-17 00:35:22 +0300

    AIBaseTask with exposed nodes

--- Source/AITesting/Private/AIBaseTask.cpp
+++ Source/AITesting/Private/AIBaseTask.cpp
@@ -7,13 +7,13 @@ void UAIBaseTask::Start()
 {
 	UE_LOG(LogTemp, Log, TEXT("UAIBaseTask::Start()"));
 	bStarted = true;
-	OnStart.Broadcast();
+	OnExecute();
 }
 
 void UAIBaseTask::Tick(float DeltaTime)
 {
 	UE_LOG(LogTemp, Log, TEXT("CustomTickable Tick"));
-	OnTick.Broadcast();
+	OnTick();
 }
 
 bool UAIBaseTask::IsTickable() const
@@ -31,6 +31,24 @@ bool UAIBaseTask::IsTickableWhenPaused() const
 	return false;
 }
 
+void UAIBaseTask::MarkCompleted()
+{
+	bCompleted = true;
+	UE_LOG(LogTemp, Log, TEXT("Task marked as completed"));
+	
+	// TODO: notify up to TaskManager
+}
+
+
+void UAIBaseTask::MarkInterrupted()
+{
+	bInterrupted = true;
+	UE_LOG(LogTemp, Log, TEXT("Task marked as interrupted"));
+	
+	// TODO: notify up to TaskManager
+}
+
+
 TStatId UAIBaseTask::GetStatId() const
 {
 	return TStatId();
@@ -38,5 +56,25 @@ TStatId UAIBaseTask::GetStatId() const
 
 UWorld* UAIBaseTask::GetWorld() const
 {
-	return GetOuter()->GetWorld();
+	// return GetOuter()->GetWorld();
+	
+	// Return null if called from the CDO, or if the outer is being destroyed
+	if (!HasAnyFlags(RF_ClassDefaultObject) &&
+		!GetOuter()->HasAnyFlags(RF_BeginDestroyed) &&
+		!GetOuter()->IsUnreachable())
+	{
+		// Try to get the world from the owning actor if we have one
+		AActor* Outer = GetTypedOuter<AActor>();
+		if (Outer != nullptr)
+		{
+			UE_LOG(LogTemp, Display, TEXT("AIBaseTask::GetWorld() => Sucessfully GetWorld()"));
+			return Outer->GetWorld();
+		}
+	}
+	
+	// Else return null - the latent action will fail to initialize
+	UE_LOG(LogTemp, Display, TEXT("World is null"));
+	return nullptr;
 }
+
+
--- Source/AITesting/Public/AIBaseTask.h
+++ Source/AITesting/Public/AIBaseTask.h
@@ -12,32 +12,43 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEventDelegate);
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable)
 class AITESTING_API UAIBaseTask : public UObject, public FTickableGameObject
 {
 	GENERATED_BODY()
 	
 public:
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnExecute();
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTick();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnInterruptedResponse();
+	
+	UFUNCTION(BlueprintCallable)
 	virtual void Start();
+
+	// TODO: keep AIC as Outer (Owner) to keep for WorldContext ?
 	
 	virtual void Tick(float DeltaTime) override;
 	virtual bool IsTickable() const override;
 	virtual bool IsTickableInEditor() const override;
 	virtual bool IsTickableWhenPaused() const override;
 
-	UPROPERTY(BlueprintAssignable, BlueprintCallable)
-	FEventDelegate OnStart;
+	UFUNCTION(BlueprintCallable)
+	virtual void MarkCompleted();
 
-	UPROPERTY(BlueprintAssignable, BlueprintCallable)
-	FEventDelegate OnTick;
-
-	UPROPERTY(BlueprintAssignable, BlueprintCallable)
-	FEventDelegate OnInterruptedResponse;
+	UFUNCTION(BlueprintCallable)
+	virtual void MarkInterrupted();
 
 	virtual TStatId GetStatId() const override;
 	virtual UWorld* GetWorld() const override;
 
 protected:
-	bool bStarted;
+	bool bStarted {false};
+	bool bCompleted {false};
+	bool bInterrupted {false};
 };
